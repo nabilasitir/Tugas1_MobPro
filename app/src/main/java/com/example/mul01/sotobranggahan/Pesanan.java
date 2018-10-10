1e93d1341a22eb835f132e0cac2aa6cf070c4a2a
nabilasitir <nabilasitir@gmail.com>
2018-10-10 17:08:28 +0700

    tes

--- app/src/main/java/com/example/mul01/sotobranggahan/Pesanan.java
+++ app/src/main/java/com/example/mul01/sotobranggahan/Pesanan.java
@@ -4,14 +4,22 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class Pesanan extends AppCompatActivity {
 
+    TextView MenuItem, JumlahItem, TotalItem, Bayar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pesanan);
-    }
+        MenuItem =  findViewById(R.id.menuitem);
+        JumlahItem = findViewById(R.id.jumlahitem);
+        TotalItem = findViewById(R.id.totalitem);
+        Bayar = findViewById(R.id.bayar);
+
+
+}
     public void Detail (View view) {
         Intent intent = new Intent(Pesanan.this, Message.class);
         startActivity(intent);
